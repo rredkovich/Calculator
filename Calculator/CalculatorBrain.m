d0be15398116f143bb4f707e9baf8bec3a339b80
Roman Redkovich <redkovich@gmail.com>
2012-07-08 14:03:34 +0400

    Old comments have been deleted

--- Calculator/CalculatorBrain.m
+++ Calculator/CalculatorBrain.m
@@ -38,54 +38,6 @@
 }
 
 
-/*-(double) popOperand {
-    NSNumber *operandObject = [self.programStack lastObject];
-    [self.programStack removeLastObject];
-    return [operandObject doubleValue];
-    }
-
--(double) performOperation:(NSString *)operation{
-    
-    double result = 0;
-    if ([operation isEqualToString:@"+"]){
-        result = self.popOperand + self.popOperand;
-        
-    } else if ([@"*" isEqualToString:operation]){
-        result = self.popOperand * self.popOperand;
-        
-    } else if ([@"-" isEqualToString:operation]){
-       double secondNumber = self.popOperand;
-       result = self.popOperand - secondNumber;
-
-    }else if ([@"/" isEqualToString:operation]) {
-        double divider = self.popOperand;
-        if (divider) {
-            result  = self.popOperand / divider;
-        }
-        
-    }else if([@"sin" isEqualToString:operation]){
-        result = sin(self.popOperand);
-        
-    }else if ([@"cos" isEqualToString:operation]){
-        result = cos(self.popOperand);
-        
-    }else if([@"sqrt" isEqualToString:operation]){
-        double radicant = self.popOperand;
-        if (radicant >= 0) {
-            result = sqrt(radicant);
-        }else{
-            result = 0;
-        }
-        
-    }
-        
-    
-    [self pushOperand:result];
-    return result;
-    
-}
-*/
-
 - (double)performOperation:(NSString *)operation
 {
     [self.programStack addObject:operation];
